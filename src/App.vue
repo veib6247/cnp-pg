ee37eaaf8793c1df50e4d13ab91e7aa9537faa48
Bryan Olandres <ozoneblacklight@outlook.com>
2023-03-21 18:08:12 +0800

    fixed custom input for getting checkout ID

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 // imports
 import { ref } from 'vue';
+
 // components
 import HeroSection from './components/HeroSection.vue';
 import UserInput from './components/UserInput.vue';
@@ -13,12 +14,6 @@ const checkoutId = ref('Thingy')
 
   <section class="container mx-auto p-10">
     <!-- get checkout ID from user -->
-    <label class="block font-semibold">Checkout ID</label>
-    <input type="text"
-      class="mt-1 block font-mono w-full rounded border-gray-300 shadow-sm focus:border-sky-300 focus:ring focus:ring-sky-300 focus:ring-opacity-10"
-      v-model="checkoutId">
-
-
     <UserInput label="Checkout ID" v-model="checkoutId" />
 
   </section>
