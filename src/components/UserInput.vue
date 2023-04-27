07c7b597c30f0de76261c5ced0789eabbaf2eb18
Bryan Olandres <ozoneblacklight@outlook.com>
2023-04-27 11:51:15 +0800

    changed input size

--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -32,7 +32,7 @@
     <label class="font-bold text-xl text-gray-700">{{ label }}</label>
 
     <input type="text"
-      class="mt-1 p-4 font-mono text-xs bg-slate-200 text-slate-800  placeholder:text-slate-400 caret-indigo-400 rounded-lg block w-full transition focus:ring focus:ring-indigo-300 focus:ring-opacity-30"
+      class="mt-1 p-4 font-mono text-xs bg-slate-200 text-slate-800  placeholder:text-slate-400 caret-indigo-400 rounded-lg block w-1/2 transition focus:ring focus:ring-indigo-300 focus:ring-opacity-30"
       :class="{ 'border-none': !isError, 'border border-red-500 focus:border-red-500': isError }" :value="modelValue"
       @keyup.enter="$emit('keyEnterAction')"
       @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)" :placeholder="placeholderText"
